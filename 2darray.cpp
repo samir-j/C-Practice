8cf2b608068f77ceeb4640f1bedcefe7540936eb
samir-j <68545603+samir-j@users.noreply.github.com>
2022-04-22 18:51:36 +0530

    Create 2darray.cpp

---
+++ 2darray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a[3][3],i,j;
+
+for(i=0; i<3; i++)
+{
+for(j=0; j<3; j++)
+{
+   cout<<"Enter the number : ";
+   cin>>a[i][j];
+
+}
+}
+ for(i=0; i<3; i++)
+ {
+    for(j=0; j<3; j++)
+    {
+    cout<<"\t"<<a[i][j];
+
+    }
+    cout<<endl;
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
