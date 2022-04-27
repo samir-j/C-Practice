49121df5b6c2865568ab2d9d2947235cd511905d
samir-j <68545603+samir-j@users.noreply.github.com>
2022-04-28 02:00:51 +0530

    Create callvalue.cpp

---
+++ callvalue.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+/*
+void swap (int l, int m){
+
+
+    int z;
+    z=l;
+    l=m;
+    m=z;
+}
+
+int main()
+{
+
+    int a,b;
+    cin>>a>>b;
+    swap(a,b);
+    cout<<a<<b;
+
+    return 0;
+
+}
+*/
+/*
+void swap(int*l , int*m){
+        int z;
+        z=*l;
+        *l=*m;
+        *m=z;
+        }
+        int  main()
+        {
+
+            int a,b;
+            cin>>a>>b;
+            swap(&a,&b);
+            cout<<a<<b;
+return 0;
+        }
+*/
+ // recursion
+ /*
+void display(int x){
+         if(x>=1){
+     cout<<x<<endl;
+     x--;
+     display (x);
+         }
+         }
+    int main(){
+    int a=20;
+    display(a);
+
+
+return 0;
+}
+
+*/
+
